0463dc2cd98f6db0730c73964d0690ec570300d7
garumble <gary.rumble@frontier.com>
2020-01-21 11:08:38 -0800

    Update CertUpdate.py

--- CertUpdate.py
+++ CertUpdate.py
@@ -2,6 +2,7 @@ import sys
 import os
 import getopt
 import logging
+import traceback
 
 from WebService import *
 
@@ -12,15 +13,25 @@ from WebService import *
 #
 def main(argv):
 	webService = WebService()
-	if len(argv) >= 3:
-		key = argv[2].replace('.' + argv[1], '')
+	if len(argv) >= 4:
+		# isolate just the domain name (input may be *.domain.com or something.domain.com)
+		x = argv[2].split('.')
+		domain = '%s.%s' % (x[len(x) - 2], x[len(x) - 1])
+		key = argv[3].replace('.' + domain, '')
 		try:
-			webService.delete(argv[1], key)
-			webService.add(argv[1], key, argv[3])
+			if argv[1] == 'delete':
+				webService.delete(domain, key)
+				return
+
+			if argv[1] == 'add':
+				webService.add(domain, key, argv[4])
+				return
+
 		except:
 			print('Operation failed\n%s' % traceback.format_exc())
-	else:
-		print("Usage: CertUpdate domain key value")
+			return
+
+	print("Usage: CertUpdate add/delete domain key value")
 
 #########################################
 if __name__ == "__main__":
